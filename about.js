34163afa3f8e20cce25eee89d3f4425436d3ac83
Kelsey Crow <kelseybcrow@gmail.com>
2021-11-12 09:30:35 -0700

    finished part 2

--- about.js
+++ about.js
@@ -1,14 +1,18 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+  // console.log("form submit");
+  alert("The form has been submitted!");
 }
 
+let compliment = (evt) => {
+  evt.preventDefault();
+  alert("You are so cool!");
+};
 
-let form = document.querySelector('form#contact');
+let form = document.querySelector("form#contact");
+form.addEventListener("submit", handleSubmit);
 
-form.addEventListener('submit', handleSubmit);
+let img = document.querySelector("img");
+img.addEventListener("mouseover", compliment);
