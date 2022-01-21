791722956cc46e181de128bfe731fc82321e08da
Kelsey Crow <kelseybcrow@gmail.com>
2022-01-21 11:23:22 -0700

    adding another js button/alert interaction

--- editing.js
+++ editing.js
@@ -1,36 +1,41 @@
 console.log("Hello world!");
 
+let question = document.querySelector('#question');
+let ritual = document.querySelector("#ritual");
+let place = document.querySelector("#place");
+let color = document.querySelector("#color");
+let yesAnswer = document.querySelector('#yes-answer')
+let noAnswer = document.querySelector('#no-answer')
+
 let handleColor = (evt) => {
   evt.preventDefault();
   alert("Blue-green");
 };
-
 let handlePlace = (evt) => {
   evt.preventDefault();
   alert("My apartment");
 };
-
 let handleRitual = (evt) => {
   evt.preventDefault();
   alert("Food-coma after a big, yummy meal");
 };
 
 let handleQuestion = (evt) => {
-  evt.preventDefault();
-  alert("Thanks for letting me know!");
-};
+  evt.preventDefault()
+  if(input === yesAnswer) {
+    alert('Awesome!') }
+  if (input === noAnswer {
+    ('You should try it!')
+  }
+}
 
-let color = document.querySelector("#color");
 color.addEventListener("click", handleColor);
 color.classList.add("big-yellow");
 
-let place = document.querySelector("#place");
 place.addEventListener("click", handlePlace);
 place.classList.add("big-yellow");
 
-let ritual = document.querySelector("#ritual");
 ritual.addEventListener("click", handleRitual);
 ritual.classList.add("big-yellow");
 
-let question = document.querySelector("#question");
-question.addEventListener("click", handleQuestion);
+question.addEventListener('click', handleQuestion)
